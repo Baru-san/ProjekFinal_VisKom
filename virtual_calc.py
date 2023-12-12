c409fea929049fc2f4702e6087dfab82a8d91a52
rendikar98 <rendikarizki2452002@gmail.com>
2023-12-12 15:08:58 +0700

    update

--- virtual_calc.py
+++ virtual_calc.py
@@ -1,6 +1,8 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 import os
+# import math
+import re
 
 class Button:
     def __init__(self, pos, width, height, value):
@@ -40,27 +42,37 @@ cap.set(3, 1280) #width
 cap.set(4, 720) #height
 detector = HandDetector(detectionCon=0.8, maxHands=1)
 
+# Get the center position of the screen
+screen_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+screen_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+center_x = screen_width / 2
+center_y = screen_height / 2
+
 #creating buttons
 
 buttonListValues = [
-                    ['7', '8', '9', '*'],
-                    ['4', '5', '6', '-'],
-                    ['1', '2', '3', '+'],
-                    ['0', '/', '.', '=']
-                    ]
+    ['7', '8', '9', '*'],
+    ['4', '5', '6', '-'],
+    ['1', '2', '3', '+'],
+    ['0', '/', '.', '='],
+    ['**', 'C'] # Clear button
+]
+
+# ...
 
 buttonList = []
-for x in range(4):
-    for y in range(4):
-        xpos = x * 100 + 800
-        ypos = y * 100 + 150
-        buttonList.append(Button((xpos, ypos), 100, 100, buttonListValues[y][x]))
-
+for i, value in enumerate(buttonListValues):
+   for j, buttonValue in enumerate(value):
+       xpos = int(center_x + i * 100 - 400) # Subtract 400 to center the buttons
+       ypos = int(center_y + j * 100 - 350) # Subtract 350 to center the buttons
+       buttonList.append(Button((xpos, ypos), 100, 100, buttonValue))
+      
 #variables
 myEquation = ''
 delayCounter = 0
 loop = True
-
+temp = ''
+tes = ''
 
 #loop
 while loop:
@@ -78,22 +90,60 @@ while loop:
                     (50, 50, 50), 3)
     for button in buttonList:
         button.draw(img)
-
     #check for hand
     if hands:
         lmList = hands[0]['lmList']
         length, _, img = detector.findDistance(lmList[8][:2], lmList[12][:2], img)
         x, y = lmList[8][:2]
-        if length<=60:
-
+        if length <= 60:
             for i, button in enumerate(buttonList):
-                if button.clicked(x, y) and delayCounter == 0 :
-                    myValue = buttonListValues[int(i%4)][int(i/4)]
-                    # print(button.value)
-                    if myValue == "=":
-                        myEquation = str(eval(myEquation))
+                # print(f"i: {i}, len(buttonList): {len(buttonList)}, len(buttonListValues): {len(buttonListValues)}")
+                if button.clicked(x, y) and delayCounter == 0:
+                    print(button.value)
+                    myValue = button.value
+                    
+                    # if myValue in ['sin', 'cos', 'tan', 'log']:
+                    #     temp = myValue
+                        
+                    
+                    if myValue == "=" and temp == '':
+                        # Check if myEquation contains both a number and an operator
+                        if re.search(r'\d', myEquation) and re.search(r'[+\-*/]', myEquation):
+                            myEquation = str(eval(myEquation))
+                        else:
+                            print("Invalid equation. Please enter a number and an operator.")
+                        
+                    # elif temp in ['sin', 'cos', 'tan', 'log'] and myValue.isdigit():
+                    #     if temp == 'sin':
+                    #         tes += myValue
+                    #         result = format(math.sin(float(tes)), '.10f')
+                    #         myEquation += temp + '(' + myValue + ')'
+                    #         myEquation = result
+                    #     elif temp == 'cos':
+                    #         result = format(math.cos(float(myValue)), '.10f')
+                    #         myEquation += temp + '(' + myValue + ')'
+                    #         myEquation = result
+                    #     elif temp == 'tan':
+                    #         result = format(math.tan(float(myValue)), '.10f')
+                    #         myEquation += temp + '(' + myValue + ')'
+                    #         myEquation = result
+                    #     # elif temp == 'log':
+                    #     #     if float(myValue) <= 0:
+                    #     #         print("Cannot compute the logarithm of zero or a negative number")
+                    #     #     else:
+                    #     #         result = format(math.log10(float(myValue)), '.10f')
+                    #     #         myEquation += temp + '(' + myValue + ')'
+                    #     #         myEquation = result
+                 
+                    elif myValue == 'C':
+                        myEquation = ''  # Clear the equation
+                        myValue = ''
+                        temp = ''
                     else:
                         myEquation += myValue
+                
+
+
                     delayCounter = 1
 
     #avoid duplicates
@@ -103,8 +153,7 @@ while loop:
             delayCounter = 0
 
     #Display result
-    cv2.putText(img, myEquation, (810, 117), cv2.FONT_HERSHEY_PLAIN, 3,
-                    (50, 50, 50), 3)
+    cv2.putText(img, myEquation, (810, 117), cv2.FONT_HERSHEY_PLAIN, 3, (50, 50, 50), 3)
 
     #Display image
     cv2.imshow("Image", img)
